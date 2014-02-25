97381c9311d90795d1ce35e29e6c2865f9dfc589
Kiernan Lamarche <kiernan.lamarche@gmail.com>
2014-02-24 22:27:28 -0700

    added EvaluatePostFixExpression1 + 2 - both tests fail

--- PostfixExprTestFixture.h
+++ PostfixExprTestFixture.h
@@ -9,12 +9,29 @@ class PostfixExprTestFixture : public CppUnit::TestFixture
 {
 public:
 	CPPUNIT_TEST_SUITE(PostfixExprTestFixture);
+	CPPUNIT_TEST(ShouldCorrectlyEvaluatePostFixExpression1);
+	CPPUNIT_TEST(ShouldCorrectlyEvaluatePostFixExpression2);
 	CPPUNIT_TEST_SUITE_END();
+private:
+	PostfixExpr* p;
+	const std::string expr1 = "10 2 4 +*";
+	const std::string expr2 = "10 2 - 7 + 3 /";
 
+public:
 	void setUp(){
+		p = new PostfixExpr();
 	}
 
 	void tearDown(){
+		delete p;
+	}
+
+	void ShouldCorrectlyEvaluatePostFixExpression1() {
+		CPPUNIT_ASSERT(p->evaluate(expr1) == 60);
+	}
+
+	void ShouldCorrectlyEvaluatePostFixExpression2() {
+		CPPUNIT_ASSERT(p->evaluate(expr2) == 5);
 	}
 };
 
--- testMain.cc
+++ testMain.cc
@@ -1,12 +1,14 @@
 #include <cppunit/ui/text/TestRunner.h>
 #include "TokenizerTestFixture.h"
 #include "StringExtensionTestFixture.h"
+#include "PostfixExprTestFixture.h"
 
 int main(void)
 {
 	CppUnit::TextUi::TestRunner runner;
 	runner.addTest(TokenizerTestFixture::suite());
 	runner.addTest(StringExtensionTestFixture::suite());
+	runner.addTest(PostfixExprTestFixture::suite());
 	runner.run();
 
 	return 0;
